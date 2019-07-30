7a60b14327d156607ab1b2f9e02d32577fd25734
ABIOLA I  OLASUPO <olasupoabiola@gmail.com>
2019-07-30 23:47:27 +0100

    modified ReditPractice

--- src/com/Practice/RedifPractice.java
+++ src/com/Practice/RedifPractice.java
@@ -15,10 +15,23 @@ public class RedifPractice {
 		//driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
 		//driver.findElement(By.className("n_tabnormal")).click();
 		//driver.findElement(By.id("srchword")).sendKeys("movies");
+		
+		
+		
 		driver.findElement(By.className("newsrchbtn")).click();
+		
 		//driver.findElement(By.className("mw001-widget-getquote-btn")).click();
 		Thread.sleep(3000);
+		
 		driver.quit();
+		
+		//driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
+		//driver.findElement(By.className("n_tabnormal")).click();
+		//driver.findElement(By.id("srchword")).sendKeys("movies");//driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
+		//driver.findElement(By.className("n_tabnormal")).click();
+		//driver.findElement(By.id("srchword")).sendKeys("movies");//driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
+		//driver.findElement(By.className("n_tabnormal")).click();
+		//driver.findElement(By.id("srchword")).sendKeys("movies");
 		
 		
 		
